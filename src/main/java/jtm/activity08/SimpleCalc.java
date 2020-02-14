6a88ce8bf57a22283dc1b8efa411e74fc86d4727
andkristine <kristine.andrejeva33@gmail.com>
2020-02-14 16:46:13 +0200

    Latest 09 SimpleCalc changes

--- src/main/java/jtm/activity08/SimpleCalc.java
+++ src/main/java/jtm/activity08/SimpleCalc.java
@@ -3,58 +3,106 @@ package jtm.activity08;
 
 public class SimpleCalc {
 
-    // TODO
+	// TODO
 	/*
-		Implement add, subtract, multiply, divide multiply methods
-		Each method should first validate input that a and b values are in range [-10,10]
-		Then perform the operation and validate result
-		If inputs and outputs are in range, return result
-		If validateInput or validateOutput method throws exception - catch it, print stacktrace and return null
+	 * Implement add, subtract, multiply, divide multiply methods Each method should
+	 * first validate input that a and b values are in range [-10,10] Then perform
+	 * the operation and validate result If inputs and outputs are in range, return
+	 * result If validateInput or validateOutput method throws exception - catch it,
+	 * print stacktrace and return null
 	 */
 
-    public static Integer add(int a, int b) {
-        // TODO implement adding operation
-		return null;
-    }
+	public static Integer add(int a, int b) {
+		// TODO implement adding operation
 
-    public static Integer subtract(int a, int b) {
-        // TODO implement subtract operation
-		return null;
-    }
+		try {
+			if (a > -10 && a < 10 && b > -10 && b < 10) {
+				return a + b;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			System.out.println("stacktrace");
+			return null;
+		}
 
-    public static Integer multiply(int a, int b) {
-        // TODO implement multiply operation
-		return null;
-    }
+	}
 
-    public static Integer divide(int a, int b) {
-        // TODO implement divide operation
-		return null;
-    }
+	public static Integer subtract(int a, int b) {
+		try {
+			if (a > -10 && a < 10 && b > -10 && b < 10) {
+				return a - b;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			System.out.println("stacktrace");
+			return null;
+		}
+
+	}
+
+	public static Integer multiply(int a, int b) {
+		try {
+			if (a > -10 && a < 10 && b > -10 && b < 10) {
+				return a * b;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			System.out.println("stacktrace");
+			return null;
+		}
+	}
+
+	public static Integer divide(int a, int b) {
+		try {
+			if (a > -10 && a < 10 && b > -10 && b < 10) {
+				return a / b;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			System.out.println("stacktrace");
+			return null;
+		}
+	}
 
 	// TODO
 	/*
-		1. Specify that method can throw SimpleCalcException
-    	2. Check that both input values a and b are in range [-10 .. 10] if either
-    		of values are out of range, throw SimpleCalcException with message
-    		Input value <value> is out of range [-10,10]
-    		where value is the value that's out of range, if a is out of range there's no
-    		need to check value b
-    */
+	 * 1. Specify that method can throw SimpleCalcException 2. Check that both input
+	 * values a and b are in range [-10 .. 10] if either of values are out of range,
+	 * throw SimpleCalcException with message Input value <value> is out of range
+	 * [-10,10] where value is the value that's out of range, if a is out of range
+	 * there's no need to check value b
+	 */
 
 	private static void validateInput(int a, int b) throws SimpleCalcException {
 
-    }
+		// try{
+
+		if (a < -10 && a > 10) {
+			throw new SimpleCalcException("Input value " + a + " is out of range [-10,10])");
+		} else if (b < -10 && b > 10) {
+			throw new SimpleCalcException("Input value " + b + " is out of range [-10,10])");
+		}
+
+//        }catch(Exception e){
+//
+//            throw new SimpleCalcException("Input value " + a + " is out of range [-10,10])"); 
+
+	}
 
 	// TODO
 	/*
-		1. Specify that method can throw SimpleCalcException
-    	2. Check that result is in range [-10 .. 10] if it's out of range,
-    		throw SimpleCalcException with message
-    		Result <result> is out of range [-10,10]
-    		where value is the result is result of executed operation
-    */
-    private static void validateOutput(int result) throws SimpleCalcException {
-		
-    }
+	 * 1. Specify that method can throw SimpleCalcException 2. Check that result is
+	 * in range [-10 .. 10] if it's out of range, throw SimpleCalcException with
+	 * message Result <result> is out of range [-10,10] where value is the result is
+	 * result of executed operation
+	 */
+	private static void validateOutput(int result) throws SimpleCalcException {
+		if (result < -10 && result > 10) {
+			throw new SimpleCalcException("Input value " + result + " is out of range [-10,10])");
+		}
+	}
 }
--- src/main/java/jtm/activity08/SimpleCalcException.java
+++ src/main/java/jtm/activity08/SimpleCalcException.java
@@ -1,5 +1,13 @@
 package jtm.activity08;
 
-public class SimpleCalcException {
+public class SimpleCalcException extends java.lang.Exception {
+
+	public SimpleCalcException(String msg) {
+		super(msg);
+	}
+
+	public SimpleCalcException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
 
 }
